002c558b967392a1eda9ff488e50e8939f59abec
Alfredo Valenzuela <jaime.valenzuela228249@potros.itson.edu.mx>
2023-02-20 12:36:24 -0700

    Se agregó el menú de los productos (practica 5)

--- app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
+++ app/src/main/java/mx/edu/itson/practica4/MenuActivity.kt
@@ -1,11 +1,20 @@
 package mx.edu.itson.practica4
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btnAntojitos: Button = findViewById(R.id.button_cold_drinks) as Button
+
+        btnAntojitos.setOnClickListener {
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
